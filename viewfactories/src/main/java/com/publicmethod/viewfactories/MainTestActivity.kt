8abf60c36bb09e27df4d971a27d37df4ed990799
Eric De Wildt <dewildte@gmail.com>
2019-07-09 20:55:28 -0400

    Add initial project files

---
+++ viewfactories/src/main/java/com/publicmethod/viewfactories/MainTestActivity.kt
@@ -0,0 +1,11 @@
+package com.publicmethod.viewfactories
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
