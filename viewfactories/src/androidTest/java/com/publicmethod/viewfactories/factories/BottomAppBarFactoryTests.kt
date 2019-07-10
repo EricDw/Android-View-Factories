d89f86e6f644522d4a731a5e3c03f805aa33d00f
Eric De Wildt <dewildte@gmail.com>
2019-07-10 00:39:29 -0400

    add support for coordinator and bottom app bar

--- viewfactories/src/androidTest/java/com/publicmethod/viewfactories/factories/BottomAppBarFactoryTests.kt
+++ viewfactories/src/androidTest/java/com/publicmethod/viewfactories/factories/BottomAppBarFactoryTests.kt
@@ -4,25 +4,27 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.publicmethod.viewfactories.BaseAndroidTests
 import com.publicmethod.viewfactories.R
 import com.publicmethod.viewfactories.TestActivity
-import org.junit.Assert.assertEquals
+import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 
+
 @RunWith(AndroidJUnit4::class)
-class FABFactoryTests : BaseAndroidTests<TestActivity>(
+class BottomAppBarFactoryTests : BaseAndroidTests<TestActivity>(
     TestActivity::class.java
 ) {
 
     @Test
-    fun fabIsGeneratedWithCorrectId() {
-        val expected = R.id.test_fab
+    fun bottomActionBarIsGeneratedWithCorrectId() {
+        val expected = R.id.test_app_bar
         activityTestRule.activity.run {
-            createFloatingActionButton {
+            createBottomAppBar {
                 id = expected
             }
         }.also {
             val actual = it.id
-            assertEquals(expected, actual)
+            Assert.assertEquals(expected, actual)
         }
     }
+
 }
